96fc42b7b3f1ff5c7eeca11afe2116a87355d31e
SiamakSamie <siamak.samie@gmail.com>
2018-04-07 16:58:51 -0400

    bump to 0.0.3-dev:wq

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,8 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                        <quick-menu :icon-class='["fa fa-graduation-cap","fa fa-book","fa fa-users","fa fa-sign-out-alt"]'
-                                    :menu-url-list='["https://github.com/AshleyLv/vue-quick-menu","https://www.npmjs.com/package/vue-quick-menu","http://www.wheelsfactory.cn/","http://www.wheelsfactory.cn/"]'>
+                        <quick-menu :icon-class='["fa fa-users", "fa fa-graduation-cap","fa fa-book","fa fa-sign-out-alt"]'
+                                    :menu-url-list='["/profile","/exams","/mentors"]'>
                         </quick-menu>
                         <polar-area :graphData='[1,2,4,5,2,5,3,3]'></polar-area>
                 </div>
